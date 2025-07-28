f6b06d82e16f0685ba315b3e951da3ab8d19616c
Hüseyin Can ÇALTI <hsyncalti2@gmail.com>
2025-07-28 08:41:48 +0300

    Health System Added

---
+++ Assets/_GameAssets/Scripts/Managers/HealthManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 3;
+
+    private int _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void Damage(int damageAmount)
+    {
+        if (_currentHealth > 0)
+        {
+            _currentHealth -= damageAmount;
+            // TODO: UI ANIMATE DAMAGE
+
+            if (_currentHealth <= 0)
+            {
+                // TODO: PLAYER DEAD
+            }
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        _currentHealth = Mathf.Min(_currentHealth + healAmount, _maxHealth);
+    }
+}
