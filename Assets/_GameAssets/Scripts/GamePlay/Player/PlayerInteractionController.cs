e7c7d4ed890b455726b8a9c099e48ee000c1da67
Hüseyin Can ÇALTI <hsyncalti2@gmail.com>
2025-08-02 13:46:24 +0300

    Camera Shake Added

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -35,6 +35,7 @@ public class PlayerInteractionController : MonoBehaviour
         if (other.TryGetComponent<IDamageable>(out var damageable))
         {
             damageable.GiveDamage(_playerRigidbody, _playerVisualTransform);
+            CameraShake.Instance.ShakeCamera(1f, 0.5f);
         }
     }
 }
